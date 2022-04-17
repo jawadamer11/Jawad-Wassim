269392f01fd6ee5af9cb6e833015a84be82154ca
Wassim Chmeis <wassimchmeis@gmail.com>
2022-04-17 17:44:52 +0300

    asd

--- app/src/main/java/com/example/jwgym/test.java
+++ app/src/main/java/com/example/jwgym/test.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class test extends AppCompatActivity {
+    static String URL = "http://192.168.0.108/ceng495";
+
+    public static String getURL() {
+        return URL;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
